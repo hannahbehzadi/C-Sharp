876371823da4914f6af40add24d574d120b4617b
Behzadi <hbehzadi@deloitte.ca>
2017-08-20 16:44:40 -0400

    Add project files.

---
+++ HewwoWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Banana;
+
+namespace HewwoWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Hannah.hello();
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace Banana
+{
+    class Hannah
+    {
+        public static void hello()
+        {
+            Console.WriteLine("Hannah Banana");
+        }
+    }
+}
